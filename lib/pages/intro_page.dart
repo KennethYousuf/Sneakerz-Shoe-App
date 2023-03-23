12c87dda094359721b45d9658d8724b225a09e70
Kenneth Yousuf <kenneth.yousuf156@gmail.com>
2023-03-22 23:42:11 -0700

    made intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,14 +8,45 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-      decoration: BoxDecoration(
-        image:DecorationImage(
-          image: AssetImage("images/1.jpg"),
-          fit: BoxFit.cover),
+      body: Stack(
+        children: [Container(
+        decoration: BoxDecoration(
+          image:DecorationImage(
+            image: AssetImage("images/1.jpg"),
+            fit: BoxFit.cover),
+            
+           ),
           
-         ),
-        
+        ),
+         Center(
+           child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Text("SN",
+                    
+               style: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 235, 19, 4)
+               ),),
+                 Text("EA",
+                    
+               style: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 235, 19, 4)
+               ),),
+                 Text("KERZ",
+                    
+               style: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 235, 19, 4)
+               ),),
+             ],
+           ),
+         )
+        ]
       ),
     );
   }
