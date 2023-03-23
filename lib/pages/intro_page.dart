f95102c29481d50eecf7877a58ea7fe77a6a3ad7
Kenneth Yousuf <kenneth.yousuf156@gmail.com>
2023-03-23 15:35:17 -0700

    Ui updated

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -8,46 +9,42 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [Container(
-        decoration: BoxDecoration(
-          image:DecorationImage(
-            image: AssetImage("images/1.jpg"),
-            fit: BoxFit.cover),
-            
-           ),
-          
+      body: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/pic1.jpg"), fit: BoxFit.cover),
+          ),
         ),
-         Center(
-           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               Text("SN",
-                    
-               style: TextStyle(
-                fontSize: 80,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 235, 19, 4)
-               ),),
-                 Text("EA",
-                    
-               style: TextStyle(
-                fontSize: 80,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 235, 19, 4)
-               ),),
-                 Text("KERZ",
-                    
-               style: TextStyle(
-                fontSize: 80,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 235, 19, 4)
-               ),),
-             ],
-           ),
-         )
-        ]
-      ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 55.0),
+              child: Text(
+                "SNEAKERZ",
+                style: GoogleFonts.anton(
+                    color: Colors.black,
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 55.0,
+              ),
+              child: Text(
+                "Brand new sneakers and custom kicks with premium quality ",
+                style: GoogleFonts.anton(
+                    color: Color.fromARGB(255, 227, 15, 15),
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal),
+              ),
+            ),
+          ],
+        )
+      ]),
     );
   }
 }
