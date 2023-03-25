4749ffc57a72776dc8ba4abfea81f3316f097913
Kenneth Yousuf <kenneth.yousuf156@gmail.com>
2023-03-25 14:21:49 -0700

    Drawer added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,54 +14,61 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-      backgroundColor: Colors.white,
-      leading: IconButton(
-        onPressed: () {
-          
-        },
-        icon: Icon(Icons.menu_rounded,
-        color: Colors.black,),
-      ),
-      actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-              Icon(Icons.home,
-            color: Colors.black,
+        backgroundColor: Colors.white,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: Icon(
+              Icons.menu_rounded,
+              color: Colors.black,
             ),
-            Icon(Icons.shopping_bag_rounded,
-            color: Colors.black,
+          );
+        }),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              Icon(
+                Icons.shopping_bag_rounded,
+                color: Colors.black,
+              ),
+            ],
+          )
+        ],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "SNEA",
+              style: GoogleFonts.anton(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "K",
+              style: GoogleFonts.anton(
+                  color: Colors.red, fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "ERZ",
+              style: GoogleFonts.anton(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
             ),
           ],
-        )
-      ],
-        title:Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    "SNEA",
-                    style: GoogleFonts.anton(
-                        color: Colors.black,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "K",
-                    style: GoogleFonts.anton(
-                        color: Colors.red,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "ERZ",
-                    style: GoogleFonts.anton(
-                        color: Colors.black,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),),
-             
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[350],
+      ),
     );
   }
 }
