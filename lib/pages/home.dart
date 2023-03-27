8b1552abf09adc858605e7b985938409a34617a0
Kenneth Yousuf <kenneth.yousuf156@gmail.com>
2023-03-26 20:13:48 -0700

    Main page in making

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -67,45 +67,87 @@ class _HomeState extends State<Home> {
         ),
       ),
       drawer: Drawer(
-        backgroundColor: Colors.grey[350],
-        child: Column(children: [
-          DrawerHeader(
-
-              child: Text(
-                'Categories',
-              style:  GoogleFonts.anton(
-                  color: Colors.black,
-                   fontSize: 28,
-                    fontWeight: FontWeight.normal),),),
-
+        backgroundColor: Color.fromARGB(255, 158, 158, 158),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  DrawerHeader(
+                    child: Text(
+                      'CATEGORIES',
+                      style: GoogleFonts.aboreto(
+                          color: Colors.black,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Divider(
+                      color: Color.fromARGB(255, 21, 20, 20),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: ListTile(
+                      title: Text(
+                        "Men",
+                        style: GoogleFonts.aBeeZee(
+                            color: Colors.black, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: ListTile(
+                      title: Text(
+                        "Women",
+                        style: GoogleFonts.aBeeZee(
+                            color: Colors.black, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Divider(
-                  color: Colors.grey,
+                padding: const EdgeInsets.only(left: 5.0, bottom: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.logout_rounded,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    "Logout",
+                    style:
+                        GoogleFonts.aBeeZee(color: Colors.black, fontSize: 18),
+                  ),
                 ),
               ),
+            ]),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.symmetric(horizontal: 25,vertical: 15),
+            
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.grey[300]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
 
-              Padding(
-                padding: const EdgeInsets.only(left:10.0),
-                child: ListTile(
-                title: Text("Men",
-                style: GoogleFonts.anton(
-                  color: Colors.black,
-                  fontSize: 18
-                ),),
-                ),
-              ),
-               Padding(
-                padding: const EdgeInsets.only(left:10.0),
-                child: ListTile(
-                title: Text("Women",
-                style: GoogleFonts.anton(
-                  color: Colors.black,
-                  fontSize: 18
-                ),),
-                ),
-              )
-        ]),
+                Text("Search",
+                style: TextStyle(
+                  color: Colors.black),),
+                Icon(Icons.search_rounded,
+                color: Colors.black,),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
